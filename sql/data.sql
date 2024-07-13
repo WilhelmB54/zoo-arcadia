53c28444f59c05daad157cc3c7076b9ddbd7dc77
Guillaume BEGENNE <gbegenne@hotmail.fr>
2024-07-13 14:49:01 +0200

    Implement US 9: Connexion system for admin, vet, and employee

---
+++ sql/data.sql
@@ -0,0 +1,77 @@
+-- Insérer les utilisateurs avec des mots de passe hashés
+INSERT INTO utilisateur (username, password, nom, prenom) VALUES
+('admin@zooarcadia.com', '$2y$10$vm9qXe5/NjQ5KiU7BZbjc.dEW05zSWz.nzqXHycrAULmvztqwRksW', 'Martin', 'Alexandre'),
+('veterinaire@zooarcadia.com', '$2y$10$xDTJa6dgGIPknagkdzHi.OmKEfQYqg6ZUneDJXYMavExMOqvuoNXK', 'Dubois', 'Sophie'),
+('employe@zooarcadia.com', '$2y$10$8hfV4YcR0QogNJBFzh6UrOOcrLLL5JKNVYaqI3ljPOo9GR4/RzCt2', 'Dupont', 'Jean');
+
+-- Insérer les rôles
+INSERT INTO role (label) VALUES
+('admin'),
+('veterinaire'),
+('employe');
+
+-- Insérer les relations utilisateurs et rôles
+INSERT INTO possede (username, role_id) VALUES
+('admin@zooarcadia.com', 1),
+('veterinaire@zooarcadia.com', 2),
+('employe@zooarcadia.com', 3);
+
+-- Insérer les services
+INSERT INTO service (nom, description) VALUES
+('Restauration', 'Profitez d''une expérience culinaire variée et de qualité lors de votre visite au Zoo Arcadia.'),
+('Visite des habitats avec un guide', 'Explorez nos habitats fascinants accompagné par nos guides experts.'),
+('Visite du zoo en petit train', 'Embarquez à bord de notre petit train pour une aventure panoramique à travers le Zoo Arcadia.');
+
+-- Insérer les habitats
+INSERT INTO habitat (nom, description, commentaire_habitat) VALUES
+('Savane', 'La savane du Zoo Arcadia représente un écosystème vaste et diversifié.', 'L''habitat de la savane semble bien adapté aux besoins de nos animaux africains. Cependant, je recommande de vérifier régulièrement l''état des points d''eau pour assurer un approvisionnement adéquat pendant les périodes sèches.'),
+('Jungle', 'Notre jungle est un éden tropical où la biodiversité foisonne parmi les frondaisons luxuriantes et les murmures des cours d''eau.', ' La végétation dense de la jungle offre un bon abri naturel pour nos animaux tropicaux. Pour améliorer leur bien-être, je propose d''enrichir les zones d''escalade et d''introduire plus d''éléments naturels pour encourager l''exploration et l''activité physique.'),
+('Marais', 'Le marais du Zoo Arcadia est un refuge tranquille pour une diversité remarquable d''animaux aquatiques.', 'Le marais offre un habitat diversifié pour nos espèces aquatiques. Pour améliorer leur confort, je recommande de vérifier régulièrement la qualité de l''eau et d''optimiser les zones d''alimentation pour soutenir leur nutrition et leur comportement naturel.');
+
+-- Insérer les animaux
+INSERT INTO animal (prenom, etat) VALUES
+('Leo', 'Bon'),
+('Tigrou', 'Bon'),
+('Paco', 'Bon'),
+('Bamboo', 'Bon'),
+('Croco', 'Bon');
+
+-- Insérer les rapports vétérinaires
+INSERT INTO rapport_veterinaire (date, detail) VALUES
+('2024-06-25', 'Leo, le lion, est en pleine forme. Il dévore !'),
+('2024-06-22', 'Tigrou, le tigre, est en bonne santé. Il aime courir vite.'),
+('2024-06-20', 'Paco, le perroquet, est très actif. Il parle tout le temps.'),
+('2024-06-24', 'Bamboo, le panda, est en pleine forme. Il aime se la couler douce.'),
+('2024-06-23', 'Croco, le crocodile, à une faim de loup. Il croque !');
+
+-- Insérer les races
+INSERT INTO race (label) VALUES
+('Lion'),
+('Tigre'),
+('Perroquet'),
+('Panda'),
+('Crocodile');
+
+-- Insérer les relations animaux et races
+INSERT INTO dispose (animal_id, race_id) VALUES
+(1, 1), -- Leo est un Lion
+(2, 2), -- Tigrou est un Tigre
+(3, 3), -- Paco est un Perroquet
+(4, 4), -- Bamboo est un Panda
+(5, 5); -- Croco est un Crocodile
+
+-- Insérer les relations animaux et rapports vétérinaires
+INSERT INTO obtient (animal_id, rapport_veterinaire_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5);
+
+-- Insérer les relations habitats et animaux
+INSERT INTO detient (habitat_id, animal_id) VALUES
+(1, 1), -- Leo dans la Savane
+(1, 2), -- Tigrou dans la Savane
+(2, 3), -- Paco dans la Jungle
+(2, 4), -- Bamboo dans la Jungle
+(3, 5); -- Croco dans le Marais
---
+++ sql/database.sql
@@ -0,0 +1,102 @@
+-- Création des tables
+CREATE TABLE utilisateur (
+    username VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE role (
+    role_id SERIAL PRIMARY KEY,
+    label VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE possede (
+    username VARCHAR(255),
+    role_id INT,
+    PRIMARY KEY (username, role_id),
+    FOREIGN KEY (username) REFERENCES utilisateur(username),
+    FOREIGN KEY (role_id) REFERENCES role(role_id)
+);
+
+CREATE TABLE service (
+    service_id SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    description VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE animal (
+    animal_id SERIAL PRIMARY KEY,
+    prenom VARCHAR(255) NOT NULL,
+    etat VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE rapport_veterinaire (
+    rapport_veterinaire_id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    detail VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE redige (
+    username VARCHAR(255),
+    rapport_veterinaire_id INT,
+    PRIMARY KEY (username, rapport_veterinaire_id),
+    FOREIGN KEY (username) REFERENCES utilisateur(username),
+    FOREIGN KEY (rapport_veterinaire_id) REFERENCES rapport_veterinaire(rapport_veterinaire_id)
+);
+
+CREATE TABLE obtient (
+    rapport_veterinaire_id INT,
+    animal_id INT,
+    PRIMARY KEY (rapport_veterinaire_id, animal_id),
+    FOREIGN KEY (rapport_veterinaire_id) REFERENCES rapport_veterinaire(rapport_veterinaire_id),
+    FOREIGN KEY (animal_id) REFERENCES animal(animal_id)
+);
+
+CREATE TABLE habitat (
+    habitat_id SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    commentaire_habitat VARCHAR(500)
+);
+
+CREATE TABLE detient (
+    habitat_id INT,
+    animal_id INT,
+    PRIMARY KEY (habitat_id, animal_id),
+    FOREIGN KEY (habitat_id) REFERENCES habitat(habitat_id),
+    FOREIGN KEY (animal_id) REFERENCES animal(animal_id)
+);
+
+CREATE TABLE race (
+    race_id SERIAL PRIMARY KEY,
+    label VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE dispose (
+    race_id INT,
+    animal_id INT,
+    PRIMARY KEY (race_id, animal_id),
+    FOREIGN KEY (race_id) REFERENCES race(race_id),
+    FOREIGN KEY (animal_id) REFERENCES animal(animal_id)
+);
+
+CREATE TABLE image (
+    image_id SERIAL PRIMARY KEY,
+    image_data BYTEA
+);
+
+CREATE TABLE avis (
+    avis_id SERIAL PRIMARY KEY,
+    pseudo VARCHAR(255) NOT NULL,
+    commentaire VARCHAR(255) NOT NULL,
+    isVisible BOOLEAN
+);
+
+CREATE TABLE comporte (
+    habitat_id INT,
+    image_id INT,
+    PRIMARY KEY (habitat_id, image_id),
+    FOREIGN KEY (habitat_id) REFERENCES habitat(habitat_id),
+    FOREIGN KEY (image_id) REFERENCES image(image_id)
+);
